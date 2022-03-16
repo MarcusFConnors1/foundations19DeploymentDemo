24f298919b8bc1b6fff5d8158543fd807631d892
Marcus Connors <biffrocker1@gmail.com>
2022-03-16 12:29:03 -0600

    message4

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get('/styles', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.css'))
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
+
+// app.get('/styles', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.css'))
+// });
 
 const port = process.env.PORT || 4005;
 
